cc4a72c84a99e175d42e76d73f31a2ea8449fe1c
Miguel Angel Tomairo <mtomairo@bcp.com.pe>
2020-05-01 18:25:13 -0500

    Initial Commit

---
+++ SearchWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchWord
+//
+//  Created by miguel tomairo on 5/1/20.
+//  Copyright © 2020 rapser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
